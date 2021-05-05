b6a367a52020d258ccb133aaad7ea2dd6336f746
HW, Siew <hw.siew@actiweb.co>
2021-05-05 13:43:10 +0800

    Added support for mongodb and mongoose for express app

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	MONGO_USER: process.env.MONGO_USER || 'root',
+	MONGO_PASSWORD: process.env.MONGO_PASSWORD || 'root',
+	MONGO_HOST: process.env.MONGO_HOST || 'mongo',
+	MONGO_PORT: process.env.MONGO_PORT || 27017,
+	MONGO_DB: process.env.MONGO_DB || '',
+}
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,24 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require('mongoose');
+const { 
+	MONGO_USER,
+	MONGO_PASSWORD, 
+	MONGO_PORT, 
+	MONGO_HOST,
+	MONGO_DB
+} = require('./config');
 
 const app = express();
+
+const connectUri = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+
+mongoose.connect(connectUri, {useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+	console.info("[mongodb] we're connected!");
+});
 
 app.use(express.static(path.join(__dirname,"public")));
 
@@ -10,5 +27,4 @@ app.get("/", (req,res) => {
 })
 
 const port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log(`listening on port ${port}`));
+app.listen(port, () => console.info(`[express] listening on port ${port}`));
